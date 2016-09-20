ea232517945309b537bdc2476f7a975bd1235046
ethanchewy <17chiue@gmail.com>
2016-09-20 12:45:29 -0400

    fixed line ordering (now starts correctly for calc. line number)

--- main/static/js/javascript.js
+++ main/static/js/javascript.js
@@ -3,10 +3,10 @@ $(document).ready(function(){
 	$('#code_textarea').bind('keypress', function(e) {
 		if(e.keyCode==13){
 			console.log('test');
-			getline();
 			$.getJSON('/check_code', {
 		      text :  $('textarea#code_textarea').val()
 		    }, function(data) {
+		    	console.log(getline());
 		    	$('#append_text').append("<h1>" + data + "</h1>");
 		    	return false;
 		    });
@@ -19,5 +19,5 @@ $(document).ready(function(){
 //Track number of rows. The number of the current row would be used to append to certain element.
 function getline() {
     var t = $("textarea")[0];
-    console.log(t.value.substr(0, t.selectionStart).split("\n").length);
+    return t.value.substr(0, t.selectionStart).split("\n").length;
 }
