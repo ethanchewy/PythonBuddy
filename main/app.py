5e4362840b5b998448775aba49593e4f3e541615
ethanchewy <17chiue@gmail.com>
2016-09-20 12:06:59 -0400

    Ajax now works

--- main/app.py
+++ main/app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return render_template("index.html")
+    
+@app.route('/check_code')
+def check_code():
+    text = request.args.get('text')
+    return jsonify(text)
--- main/static/js/javascript.js
+++ main/static/js/javascript.js
@@ -1,7 +1,15 @@
+//jQuery all client side
 $(document).ready(function(){
-$('#code_textarea').bind('keypress', function(e) {
-	if(e.keyCode==13){
-		console.log('test');
-	}
-});
+	$('#code_textarea').bind('keypress', function(e) {
+		if(e.keyCode==13){
+			console.log('test');
+			$.getJSON('/check_code', {
+		      text :  $('textarea#code_textarea').val()
+		    }, function(data) {
+		    	console.log("hi");
+		      //$('#append_text').replaceWith(text);
+		    });
+		    return false;
+		  };
+	});
 });
