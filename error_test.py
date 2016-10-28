1e684002dd569c689273d807085c9f51db5ed1ec
ethanchewy <17chiue@gmail.com>
2016-10-28 11:17:30 -0400

    gif

--- error_test.py
+++ error_test.py
@@ -1,6 +1,4 @@
-methods = []                       
-for i in range(10):                        
-    methodds.append(lambda x: x + i)        
-
+methods = []
+for i in range(10):
+    methodds.append(lambda x: x + i)
 print methods[0](10)
-    
