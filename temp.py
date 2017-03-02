16a3f8fc81705d11ccba9d09e322199825e6d18c
Ethan Chiu <ethanchewy@users.noreply.github.com>
2017-03-01 22:17:50 -0500

    Fix Security Holes
    http://stackoverflow.com/questions/1350466/preventing-python-code-from-importing-certain-modules

--- temp.py
+++ temp.py
@@ -1,2 +1,13 @@
-for i in range(5):
-    print i
+import sys
+sys.modules['os']=None
+sys.modules['os.path']=None
+sys.modules['pprint']=None
+sys.modules['builtins']=None
+sys.modules['shutil']=None
+sys.modules['subprocess']=None
+sys.modules['jinja2']=None
+sys.modules['subprocess']=None
+sys.modules['subprocess']=None
+sys.modules['yaml']=None
+sys.modules['sys']=None
+#STOP
