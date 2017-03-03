72800647ffb95ceeaef1ce2f6ac0a3310fc26aeb
ethanchewy <17chiue@gmail.com>
2017-03-03 17:48:42 -0500

    fix js errors

--- venv/lib/python2.7/site-packages/gunicorn/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -
-#
-# This file is part of gunicorn released under the MIT license.
-# See the NOTICE for more information.
-
-version_info = (0, 14, 2)
-__version__ = ".".join(map(str, version_info))
-SERVER_SOFTWARE = "gunicorn/%s" % __version__
